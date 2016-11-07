e721fb3f49471185d10fcba1700af8117dfc688e
Javier <Javier@Javier-Portatil>
2016-11-07 11:55:39 +0000

    Version1

---
+++ src/kata4/Kata4.java
@@ -0,0 +1,13 @@
+//Alberto Hernández Rodríguez
+package kata4;
+import java.io.File;
+public class Kata4 {
+    public static void main(String[] args) {
+        File file=new File("C:\\Users\\Javier\\Desktop\\universidad");
+        String[]names=file.list();
+        for(String name:names){
+            System.out.println(name);
+        }
+    }
+    
+}
